b23caeed4f4d0ea1affe23a32692ac610a810934
edwin rivera <waehake@gmail.com>
2018-01-30 14:44:26 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,13 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default DigitalClicker;
