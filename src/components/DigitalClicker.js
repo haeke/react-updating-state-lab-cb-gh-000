1ae0efcc0099e18e331712ead8bc2505a989b019
edwin rivera <waehake@gmail.com>
2018-01-30 14:44:56 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,9 +2,16 @@
 import React, { Component } from 'react';
 
 class DigitalClicker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0,
+    };
+  }
   render() {
     return (
       <div>
+      
       </div>
     );
   }
